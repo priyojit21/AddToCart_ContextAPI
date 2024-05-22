61162725dc81580bc2202227f08f65e99129764d
priyojit21 <102255292+priyojit21@users.noreply.github.com>
2024-05-22 21:19:34 +0530

    some_Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import Item from "./components/Item";
 import Bill from "./components/Bill";
+import { useCart } from "./context/Cart";
 function App() {
 
 
+  const Cart = useCart();
+
+  const {items} = Cart;
+
   return (
     <>
-      <div>
+    <div style={{display:"flex"}}>
+    <div>
         <Item name="Macbook Pro" price="100000" />
         <Item name="Sandisk Pendrive" price="2000" />
         <Item name="Iphone 13" price="54000" />
         <Item name="Svaar Clogs" price="500" />
         <Bill/>
       </div>
+      <div>
+        <p>Total : {items.length}</p>
+      </div>
+    </div>
+      
     </>
   )
 }
